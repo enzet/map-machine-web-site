674e3817dec96466b9de51eb29001cc73bee223f
Sergey Vartanov <me@enzet.ru>
2021-12-18 00:25:33 +0300

    Issue #1: add flipping icon section
    Surveillance icon is flipped horizontally when it is directed to the
    west.

---
+++ roentgen/code.js
@@ -0,0 +1,68 @@
+window.setInterval(runDirection, 1);
+
+directionSector = document.getElementById("direction_sector");
+directionIcon = document.getElementById("direction_icon");
+directionText = document.getElementById("direction_text");
+
+// Initialization.
+
+modeIndex = 0;
+directionIterator = 0;
+
+function descartes(degree, center, radius) {
+    return {
+        x: center.x + Math.cos(degree / 180 * Math.PI) * radius,
+        y: center.y + Math.sin(degree / 180 * Math.PI) * radius
+    };
+}
+
+function toCoordinates(vector) {
+    return vector.x + "," + vector.y;
+}
+
+function smooth(value) {
+    return Math.sin(value * Math.PI / 2);
+}
+
+function runDirection() {
+
+    minSectorSize = 10;
+    maxSectorSize = 25;
+    radius = 120;
+    center = {x: 150, y: 140};
+
+    directionIterator = (directionIterator < 360) ? directionIterator + 0.8 : 0
+
+    // Convert to [-1, 1].
+    if (directionIterator >= 90 && directionIterator <= 270) {
+        d = (directionIterator - 180) / 90;
+    } else {
+        d = (d >= 270) ? directionIterator / 90 : (directionIterator - 360) / 90
+    }
+    d = smooth(smooth(d));
+    speed = Math.abs(d);
+
+    // Convert back.
+    if (directionIterator >= 90 && directionIterator <= 270) {
+        d = d * 90 + 180;
+    } else {
+        d = (d >= 0) ? d * 90 : d * 90 + 360;
+    }
+    size = maxSectorSize - speed * (maxSectorSize - minSectorSize);
+    directionText.innerHTML = "direction: " + Math.floor(d);
+    degree = d - 90;
+
+    scale = directionIterator > 180 ? "-4,4" : "4";
+    point1 = descartes(degree - size, center, radius);
+    point2 = descartes(degree + size, center, radius);
+
+    directionSector.setAttribute(
+        "d",
+        "M " + toCoordinates(point1) + " L " + toCoordinates(center) + " L "
+        + toCoordinates(point2)
+    )
+    directionIcon.setAttribute(
+        "transform",
+        "translate(150,150) scale(" + scale + ") translate(-584,-24)"
+    )
+}
