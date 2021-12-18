d38f5b56b2401f5e38b37787136bc5524cbdb961
Sergey Vartanov <me@enzet.ru>
2021-12-19 01:03:32 +0300

    Issue #3: fix grid section

--- roentgen/grid.js
+++ roentgen/grid.js
@@ -1,4 +1,4 @@
-window.setInterval(gridRun, 25);
+window.setInterval(gridRun, 10);
 
 gridIterator = 0
 gridCenter = [150, 150]
@@ -21,14 +21,44 @@ for (i in [...Array(30).keys()]) {
 function gridRun() {
     gridIterator = (gridIterator < 300) ? gridIterator + 1 : 0
 
-    for (i in [...Array(15).keys()]) {
-        point1 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, gridIterator - 50 - 250])
-        point2 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, gridIterator + 50 - 250])
-        gridLines[i].setAttribute("d", line(point1, point2))
-    }
-    for (i in [...Array(15).keys()]) {
-        point1 = plus(gridCenter, [-gridIterator, (parseInt(i) - 7) * gridScale])
-        point2 = plus(gridCenter, [gridIterator, (parseInt(i) - 7) * gridScale])
-        gridLines[parseInt(i) + 15].setAttribute("d", line(point1, point2))
+    for (i in [...Array(30).keys()]) {
+
+        if (i < 15) {
+            localIterator = gridIterator + i * 2
+        } else {
+            localIterator = gridIterator + (i - 15) * 2 // Try also (i - 35).
+        }
+
+        if (localIterator > 300) {
+            localIterator -= 300
+        }
+
+        if (localIterator <= 100 || localIterator >= 200) {
+            s = (smooth(smooth((localIterator / 50) - 1)) + 1) / 2
+            if (i < 15) {
+                if (localIterator <= 100) {
+                    point1 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, 50 * (s - 1) - 7 * 12])
+                    point2 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, 50 * (s - 1) + 7 * 12])
+                } else if (localIterator <= 300) {
+                    point1 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, 50 * s - 7 * 12])
+                    point2 = plus(gridCenter, [(parseInt(i) - 7) * gridScale, 50 * s + 7 * 12])
+                }
+            } else {
+                if (localIterator <= 100) {
+                    point1 = plus(gridCenter, [50 * (s - 1) - 7 * 12, (parseInt(i) - 15 - 7) * gridScale, ])
+                    point2 = plus(gridCenter, [50 * (s - 1) + 7 * 12, (parseInt(i) - 15 - 7) * gridScale, ])
+                } else if (localIterator <= 300) {
+                    point1 = plus(gridCenter, [50 * s - 7 * 12, (parseInt(i) - 15 - 7) * gridScale, ])
+                    point2 = plus(gridCenter, [50 * s + 7 * 12, (parseInt(i) - 15 - 7) * gridScale, ])
+                }
+            }
+            gridLines[i].setAttribute("d", line(point1, point2))
+
+            if (localIterator <= 100) {
+                gridLines[i].style.strokeWidth = Math.abs(s / 3)
+            } else if (localIterator <= 300) {
+                gridLines[i].style.strokeWidth = Math.abs(1 / 3 - s / 3)
+            }
+        }
     }
 }
