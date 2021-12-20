ee4b8574bbd50e5d76a12616961a4aed7815900d
Sergey Vartanov <me@enzet.ru>
2021-12-21 00:21:45 +0300

    Issue #2: move lines to the background

--- roentgen/size.js
+++ roentgen/size.js
@@ -22,7 +22,7 @@ for (i in [...Array(4).keys()]) {
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.style.stroke = "#555";
     path.style.strokeWidth = "1px";
-    sizeCanvas.appendChild(path);
+    sizeCanvas.prepend(path);
 
     sizeLines[i] = path;
 }
