e3c2c0e4dad43009ebfeb375a5c485b8141eea96
Sergey Vartanov <me@enzet.ru>
2021-12-22 02:42:27 +0300

    Close #10: fix size lines

--- roentgen/size.js
+++ roentgen/size.js
@@ -4,7 +4,8 @@ sizeIterator = 0
 sizeModeIndex = 0
 minIconSize = 14
 maxIconSize = 128
-sizeCenter = [150.0, 150.0]
+sizeCenter = [150.5, 150.5]
+tipLength = 10.0
 sizeIcons = [
     icons.diving_platform,
     icons.wind_turbine,
@@ -37,14 +38,15 @@ function changeGridOpacity(opacity) {
 function sizeRun() {
 
     if (sizeIterator == 0 && sizeModeIndex == 0) {
-        sizeIcon = sizeIcons[sizeIconIndex]
-        sizeIconFill.setAttribute("d", sizeIcon.d)
-        sizeIconIndex = sizeIconIndex < sizeIcons.length - 1 ? sizeIconIndex + 1 : 0
+        sizeIcon = sizeIcons[sizeIconIndex];
+        sizeIconFill.setAttribute("d", sizeIcon.d);
+        sizeIconIndex =
+            sizeIconIndex < sizeIcons.length - 1 ? sizeIconIndex + 1 : 0;
         sizeIconFill.setAttribute(
             "transform",
-            "translate(150,150) "
+            "translate(" + toCoordinates(sizeCenter) + ") "
             + "translate(" + sizeIcon.x + "," + sizeIcon.y + ")"
-        )
+        );
     }
 
     [sizeIterator, sizeModeIndex] =
@@ -68,22 +70,23 @@ function sizeRun() {
 
         sizeIconFill.setAttribute(
             "transform",
-            "translate(150,150) scale(" + sizeScale + ") "
+            "translate(" + toCoordinates(sizeCenter) + ") "
+            + "scale(" + sizeScale + ") "
             + "translate(" + sizeIcon.x + "," + sizeIcon.y + ")"
         )
         s = sizeScale * minIconSize / 2
 
         p = [
-            [[-s - 20, -s], [s + 20, -s]],
-            [[-s - 20, s], [s + 20, s]],
-            [[-s, -s - 20], [-s, s + 20]],
-            [[s, -s - 20], [s, s + 20]],
+            [[-s - tipLength, -s], [s + tipLength, -s]],
+            [[-s - tipLength, s], [s + tipLength, s]],
+            [[-s, -s - tipLength], [-s, s + tipLength]],
+            [[s, -s - tipLength], [s, s + tipLength]],
         ]
         for (i in [...Array(4).keys()]) {
             sizeLines[i].setAttribute("d",
                 line(plus(sizeCenter, p[i][0]), plus(sizeCenter, p[i][1])))
         }
-        sizeText.setAttribute("x", 150 + s + 70)
+        sizeText.setAttribute("x", sizeCenter[0] + s + 70)
         sizeText.innerHTML = Math.round(s * 2) + " px"
     } else if (sizeMode.action == "fade_in") {
         sizeIconFill.style.opacity = sizeScale
